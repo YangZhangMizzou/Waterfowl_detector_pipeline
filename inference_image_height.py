3f3eacdc3e8c9082b138216d3051779204b9b490
YangZhangMizzou <yangzhang@missouri.edu>
2023-10-02 22:32:40 -0500

    Update inference_image_height.py

--- inference_image_height.py
+++ inference_image_height.py
@@ -38,7 +38,6 @@ from models.common import DetectMultiBackend
 from utils.general import (LOGGER, check_file, check_img_size, check_imshow, check_requirements, colorstr, cv2,
                            increment_path, non_max_suppression, print_args, scale_coords, strip_optimizer, xyxy2xywh)
 
-
 #yolonas
 import super_gradients
 
